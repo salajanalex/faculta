4c76e8d03bda2675e425ed33e86220ed8e318904
salajanalex <alex_sala96cs@yahoo.com>
2018-03-23 13:58:39 +0200

    Lab MPP Tryhard

--- problema11/src/main/java/ro/salajan/repository/ParticipantRepo.java
+++ problema11/src/main/java/ro/salajan/repository/ParticipantRepo.java
@@ -3,7 +3,9 @@ package ro.salajan.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ro.salajan.model.Participant;
 
-public interface ParticipantRepo extends JpaRepository<Participant, Integer> {
+import java.util.List;
 
+public interface ParticipantRepo extends JpaRepository<Participant, Integer> {
+    public List<Participant> findByEchipa(String echipa);
 
 }
--- problema11/src/test/java/CrudTest.java
+++ problema11/src/test/java/CrudTest.java
@@ -1,6 +1,6 @@
 import static org.junit.Assert.*;
 
-import ro.salajan.config.Application;
+import ro.salajan.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
